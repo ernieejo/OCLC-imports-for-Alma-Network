031f65ff84615a4bc7d4901e7780450972e1467e
Erin Johnson <36278045+ernieejo@users.noreply.github.com>
2024-02-12 12:26:59 -0500

    Update OCLC-Comparison File.py

--- OCLC-IZ Comparison File.py
+++ OCLC-IZ Comparison File.py
@@ -3,24 +3,28 @@ from pandas import DataFrame, ExcelWriter
 import csv
 
 #Read the BIB processing report
-data = pd.read_csv('C://...//OCLC Imports//BibProcessingReport.txt', sep="|", header=None)
-data.columns = ["a", "b", "c", "d", "e"]
+data = pd.read_csv('BibProcessingReport.txt', sep="|", header=None, dtype=str)
+data.columns = ["JobID", "MMSID", "Existing 035a", "Incoming 035a", "Action"]
 
 #make a dataframe from the BIB processing report and set the format as text for columns b and c
-df = pd.DataFrame(data, columns= ['a', 'b', 'c', 'd', 'e'])
-df['b']= df['b'].astype(str)
+df = pd.DataFrame(data, columns= ['JobID', 'MMSID', 'Existing 035a', 'Incoming 035a', 'Action'])
+df['MMSID']= df['MMSID'].astype(str)
 
 #Define the sheets that we will have in the comparison_fileIZ
-DIFF = df[df['c'] != df['d']]
-SAME = df[df['c'] == df['d']]
+DIFF = df[df['Existing 035a'] != df['Incoming 035a']]
+SAME = df[df['Existing 035a'] == df['Incoming 035a']]
 
 print(DIFF)
 print(SAME)
 
 #Create the comparison_fileIZ file
-writer = pd.ExcelWriter('C://...//OCLC Imports//comparison_fileIZ.xlsx', engine='xlsxwriter')
-DIFF.to_excel(writer, sheet_name='DIFF')
-SAME.to_excel(writer, sheet_name='SAME')
+writer = pd.ExcelWriter('comparison_file.xlsx', engine='xlsxwriter')
+DIFF.to_excel(writer, index=False, sheet_name='DIFF')
+SAME.to_excel(writer, index=False, sheet_name='SAME')
+workbook = writer.book
+worksheet = writer.sheets['DIFF']
+text_fmt = workbook.add_format({'num_format': '@'})
+worksheet.set_column('B:B',20, text_fmt)
 writer.save()
 
 
